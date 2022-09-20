123977b348a49bfcf8e86a63149df1bfe1d7e764
Arslan Sarwar <masarwar171@gmail.com>
2022-09-20 12:05:09 +0500

    Navbar in Refine App

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
 import { Refine } from "@pankod/refine-core";
+import React from "react";
+import 'App.css'
+import 'index.css';
 import routerProvider from "@pankod/refine-react-router-v6";
 import dataProvider from "@pankod/refine-simple-rest";
+import { postList } from "pages/posts";
+import { categoryList } from "pages/categories";
+import { userList } from "pages/users";
+import { eventList } from "pages/events";
+import { Layout } from "components/Layout";
 
-function App() {
-  return (
-    <Refine
-      routerProvider={routerProvider}
-      dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
-    />
-  );
-}
+const App: React.FC = () => {
+    return (
+        <Refine
+            routerProvider={routerProvider}
+            dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
+            resources={[{ name: "posts", list: postList }, { name: "categories", list: categoryList }, { name: "users", list: userList }, { name: "events", list: eventList }]}
+            Layout={Layout}
+        />
+    );
+};
 
 export default App;
